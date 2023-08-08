069cdf61358d26ac7558e3de104447a4ecb5fed0
YakoViTo <kavirjosegermanav@gmail.com>
2023-08-08 18:10:35 -0400

    data import and export

--- ManageWorkers/admin.py
+++ ManageWorkers/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import Afiliado
+from import_export import resources
 
-# Register your models here.
+class AfiliadoResource(resources.ModelResource):
+    class Meta:
+        model = Afiliado
+        
+class AfiliadoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display=("id", "nombre", "cod_cargo", "profession", "municipio", "cod_plantel", "empresa")
+    search_fields=("id", "nombre", "cod_cargo", "profession", "municipio", "cod_plantel", "empresa")
+    list_filter=("profession", "municipio", "empresa")
+    resource_class = AfiliadoResource
+
+admin.site.register(Afiliado, AfiliadoAdmin)
--- ManageWorkers/models.py
+++ ManageWorkers/models.py
@@ -1,20 +1,18 @@
 from django.db import models
-from django.core.validators import MinValueValidator
 
 # Create your models here.
 class Afiliado(models.Model):# Creación de la tabla con el nombre Afiliado
     # Campos de la tabla
     id = models.CharField(primary_key=True, max_length=20, verbose_name="Cédula de Identidad")
-    nombre=models.CharField(max_length=20, verbose_name="Apellidos y Nombres")
-    cod_cargo=models.PositiveIntegerField(validators=[MinValueValidator(1)],verbose_name="Cod_Cargo")
+    nombre=models.CharField(max_length=50, verbose_name="Apellidos y Nombres")
+    cod_cargo=models.CharField(max_length=50,verbose_name="Cod_Cargo")
     profession=models.CharField(max_length=50, verbose_name="Profesión u Oficio")
     municipio=models.CharField(max_length=20, verbose_name="Municipio")
-    cod_plantel=models.PositiveIntegerField(validators=[MinValueValidator(1)],verbose_name="Cod_Plantel")
+    cod_plantel=models.CharField(max_length=50,verbose_name="Cod_Plantel")
     empresa=models.CharField(max_length=50, verbose_name="Empresa u organismo donde trabaja")
-    #estatus = models.CharField(max_length=30, default='Habilitado')
 
     def __str__(self):
-        return self.id
+        return str(self.id)
 
     class Meta:
         verbose_name='afiliado'
