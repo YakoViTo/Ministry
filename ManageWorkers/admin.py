a31df4555d8756288c4d778df08caf68957167d2
YakoViTo <kavirjosegermanav@gmail.com>
2023-09-02 15:36:38 -0400

    improving some things

--- ManageWorkers/admin.py
+++ ManageWorkers/admin.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from .models import Afiliado
 from import_export import resources
+from django.utils.html import format_html
+from django_admin_listfilter_dropdown.filters import (
+    DropdownFilter, ChoiceDropdownFilter, RelatedDropdownFilter
+)
 
-admin.site.site_header = "SNFPMEANZ"
+# Personalización del encabezado del sitio de administración
+admin.site.site_header = format_html('<img src="/static/img/logo-m3.jpg" alt="Ícono"> SNFPMEANZ')
+admin.site.site_title = "SNFPMEANZ"
 admin.site.index_title = "Sistema de gestión de afiliados al Sindicato Nacional de Funcionarios Públicos del Ministerio de Educación"
 admin.site.site_url = "https://www.snfpmeanz.com/"
 
+# Definición de un recurso para importar y exportar datos
 class AfiliadoResource(resources.ModelResource):
     def before_import_row(self, row, **kwargs):
         # Aplicar limpieza a los datos en la fila antes de importarla
@@ -15,21 +22,27 @@ class AfiliadoResource(resources.ModelResource):
         row['profession'] = row['profession'].strip()
         row['municipio'] = row['municipio'].strip()
         row['cod_plantel'] = str(row['cod_plantel']).strip()
-        row['empresa'] = row['empresa'].strip()
+        row['plantel'] = row['plantel'].strip()
 
     class Meta:
         model = Afiliado
 
+# Configuración personalizada para el administrador
 class AfiliadoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    list_display = ("id", "nombre", "cod_cargo", "profession", "municipio", "cod_plantel", "empresa", "cuota", "formatted_fecha")
-    search_fields = ("id", "nombre", "cod_cargo", "profession", "municipio", "cod_plantel", "empresa", "cuota", "formatted_fecha")
-    list_filter = ("profession", "municipio", "empresa")
+    
+    list_display = ("id", "nombre", "cod_cargo", "profession", "municipio", "cod_plantel", "plantel", "cuota", "formatted_fecha")
+    search_fields = ("id", "nombre", "cod_cargo", "profession", "municipio", "cod_plantel", "plantel", "cuota")
+    list_filter = (
+        ('profession', DropdownFilter),
+        ('municipio', DropdownFilter),
+        ('plantel', DropdownFilter),
+    )
     date_hierarchy = "fecha"
     resource_class = AfiliadoResource
-    list_per_page = 50
+    list_per_page = 25
     
     def formatted_fecha(self, obj):
         return obj.fecha.strftime('%d-%m-%Y %H:%M')
     formatted_fecha.short_description = 'Fecha de Ingreso'
-
+# Registrar el modelo Afiliado con la configuración personalizada en el sitio de administración
 admin.site.register(Afiliado, AfiliadoAdmin)
--- Ministry/settings.py
+++ Ministry/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #'admin_interface',
+    #'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ManageWorkers',
     'import_export',
+    'django_admin_listfilter_dropdown',
 ]
+
+X_FRAME_OPTIONS= 'SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,7 +61,7 @@ ROOT_URLCONF = 'Ministry.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,9 +122,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# Configura la ruta del favicon
+FAVICON_PATH = STATIC_URL + 'img/favicon.ico'
+
+# Directorio donde se almacenarán los archivos estáticos recopilados en producción
+#STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#MEDIA_URL='/media/'
+#MEDIA_ROOT=BASE_DIR / 'media'
